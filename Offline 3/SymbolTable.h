b9e5af9e9a04f14a79d3ceee46bafd4d554b08a0
ababiltalha <1805077@ugrad.cse.buet.ac.bd>
2022-06-25 13:25:57 +0600

    Offline 3 started

---
+++ Offline 3/SymbolTable.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<bits/stdc++.h>
+#include "ScopeTable.cpp"
+using namespace std;
+
+class SymbolTable
+{
+private:
+    int bucket;
+    ScopeTable* currentScope;
+    int globalId;
+
+public:
+    SymbolTable(int bucket);
+    ~SymbolTable();
+    void enterScope(); // S
+    void exitScope(); // E
+    bool insertSymbol(string name, string type); // I
+    bool removeSymbol(string name); // D
+    SymbolInfo* lookUpSymbol(string name); // L
+    void printCurrentScope(); // P C
+    string printAllScope(); // P A
+
+
+};
+
